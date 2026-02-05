2226e1912acc29463efff50ca5dd725d2c719c3c
admin <anhkoquen7@gmail.com>
2026-02-05 12:45:48 +0700

    log experiment on mlflow

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,7 @@
 from cnnClassifier.constants import *
 import os
 from cnnClassifier.utils.common import read_yaml, create_directories, save_json
-from cnnClassifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig)
+from cnnClassifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig, EvaluationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -72,3 +72,13 @@ class ConfigurationManager:
         )
         return training_config
     
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/kidney_data",
+            mlflow_uri="https://dagshub.com/toannct10/Kidney-Disease-Classification-DeepLearning-Project.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
